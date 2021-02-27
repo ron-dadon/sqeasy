38b4c0b77485b7b3d849c696da0440ea153970f4
Ron Dadon <rondadon1@gmail.com>
2021-02-27 21:33:31 +0200

    Add isErrorHandlingMiddleware util

--- src/utils.js
+++ src/utils.js
@@ -6,7 +6,12 @@ function secondsToMilliseconds(sec) {
   return sec * 1000
 }
 
+function isErrorHandlingMiddleware(fn) {
+  return fn.length >= 3
+}
+
 module.exports = {
   isFunction,
-  secondsToMilliseconds
+  secondsToMilliseconds,
+  isErrorHandlingMiddleware
 }
--- tests/utils.spec.js
+++ tests/utils.spec.js
@@ -1,9 +1,10 @@
-const { isFunction, secondsToMilliseconds } = require('../src/utils.js')
+const { isFunction, secondsToMilliseconds, isErrorHandlingMiddleware } = require('../src/utils.js')
 
 describe('isFunction', function() {
   it('should return true if argument is a function', function() {
     expect(isFunction(function() {})).toBeTruthy()
   })
+
   it('should return false if argument is not a function', function() {
     expect(isFunction({})).toBeFalsy()
   })
@@ -12,5 +13,18 @@ describe('isFunction', function() {
 describe('secondsToMilliseconds', function() {
   it('should return milliseconds of seconds', function() {
     expect(secondsToMilliseconds(1)).toEqual(1000)
+  })
+})
+
+describe('isErrorHandlingMiddleware', function() {
+  it('should return true if function takes 3 or more arguments', function() {
+    expect(isErrorHandlingMiddleware(function(a, b, c) {})).toBeTruthy()
+    expect(isErrorHandlingMiddleware(function(a, b, c, d) {})).toBeTruthy()
+  })
+
+  it('should return true if function takes 2 or less arguments', function() {
+    expect(isErrorHandlingMiddleware(function(a, b) {})).toBeFalsy()
+    expect(isErrorHandlingMiddleware(function(a) {})).toBeFalsy()
+    expect(isErrorHandlingMiddleware(function() {})).toBeFalsy()
   })
 })
